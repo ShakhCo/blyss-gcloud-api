807ff57b1ce6c9b0e251c470aefdc09511e189ce
ShakhCo <ssharipov3@students.wiut.uz>
2025-12-17 19:48:50 +0500

    Add CORS support in server.js and update package.json and package-lock.json to include cors dependency

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import multer from 'multer';
 import routes from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const upload = multer();
+
+app.use(cors({
+    origin: 'https://barbershop-miniapp-beta.automations.uz',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
