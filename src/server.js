679439506a7325ce2fc0512392a4a9598c849053
ShakhCo <ssharipov3@students.wiut.uz>
2026-01-25 00:08:54 +0500

    Remove unused file upload middleware from server.js to streamline server setup.

--- src/server.js
+++ src/server.js
@@ -26,7 +26,6 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(upload.none());
 
 app.get('/', (req, res) => {
     res.send('Hello world');
